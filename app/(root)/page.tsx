09a81ca0af92a405a0ee577513b54445d7e61f9a
sardorazimov <sardorazimov2901@gmail.com>
2025-02-20 23:59:31 +0300

    (delete_project)

--- app/(root)/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-import React from 'react'
-
- export  const page = () => {
-  return redirect('/home')
-}
-
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/themes";
-import Navbar from "@/components/shared/Navbar";
-import MobileFooter from "@/components/shared/detalis/MobileFooter";
-import Sidebar from "@/components/shared/SideBar";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
